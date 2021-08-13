fbef2755055fda5a1ee75ec875c85612f0c4df9e
jianylei <jianyonglei@gmail.com>
2021-08-13 14:18:38 -0400

    replaced <hr> with <br> for more modernized look

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
 /************************************************************************ *********
-* WEB322 – Assignment 05
+* WEB322 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students. *
-* Name: Danny Student ID: 164700197 Date: 07/30/21 *
+* Name: Danny Student ID: 164700197 Date: 08/10/21 *
 * Online (Heroku) Link: https://webapp-web322.herokuapp.com
 * ********************************************************************************/
 const dataServiceAuth = require("./data-service-auth.js");
